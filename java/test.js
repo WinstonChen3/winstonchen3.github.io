a0dd04a3a6632800aea77c897048bf228ac78df7
WinstonChen3 <150869473+WinstonChen3@users.noreply.github.com>
2023-11-16 06:52:59 -0600

    a

--- java/test.js
+++ java/test.js
@@ -18,3 +18,63 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3500); // Change image every 3.5 seconds
 }
+const signupBtn = document.getElementById('signupBtn');
+const loginBtn = document.getElementById('loginBtn');
+const popup = document.getElementById('popup');
+const popupTitle = document.getElementById('popupTitle');
+const popupForm = document.getElementById('popupForm');
+const closeBtn = document.getElementById('closeBtn');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+signupBtn.addEventListener('click', () => {
+  popupTitle.textContent = 'Sign Up';
+  popupForm.addEventListener('submit', handleSignup);
+  popup.style.display = 'block';
+});
+
+loginBtn.addEventListener('click', () => {
+  popupTitle.textContent = 'Login';
+  popupForm.addEventListener('submit', handleLogin);
+  popup.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  closePopup();
+});
+
+function handleSignup(event) {
+  event.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  
+  // Perform signup logic here
+  // You can send the data to a server or perform client-side processing
+  
+  console.log('Sign up:', username, password);
+  
+  // Close the popup
+  closePopup();
+}
+
+function handleLogin(event) {
+  event.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  
+  // Perform login logic here
+  // You can send the data to a server or perform client-side processing
+  
+  console.log('Login:', username, password);
+  
+  // Close the popup
+  closePopup();
+}
+
+function closePopup() {
+  popupForm.removeEventListener('submit', handleSignup);
+  popupForm.removeEventListener('submit', handleLogin);
+  popup.style.display = 'none';
+  usernameInput.value = '';
+  passwordInput.value = '';
+}
