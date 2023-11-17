20bcc50878aaf066a2e827d6ec3cfbc532346ce0
WinstonChen3 <150869473+WinstonChen3@users.noreply.github.com>
2023-11-17 00:12:28 +0000

    a

--- java/backend.js
+++ java/backend.js
@@ -1,40 +1,90 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const port = 3000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error('MongoDB connection error:', error));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: true,
+  saveUninitialized: true
+}));
 
-// Create a user schema
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-// Create a user model
-const User = mongoose.model('User', userSchema);
-
-// Handle login POST request
-app.post('/login', (req, res) => {
+app.post('/register', (req, res) => {
   const { username, password } = req.body;
 
-  // Find the user in the database
-  User.findOne({ username: username, password: password }, (error, user) => {
-    if (error) {
-      res.status(500).json({ success: false });
-    } else if (user) {
-      res.json({ success: true });
-    } else {
-      res.json({ success: false });
-    }
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) return console.error(err);
+
+    const newUser = {
+      username,
+      password: hash
+    };
+
+    db.collection('users').insertOne(newUser, (err) => {
+      if (err) return console.error(err);
+
+      res.send('Registration successful');
+    });
   });
 });
 
-// Start the server
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  db.collection('users').findOne({ username }, (err, user) => {
+    if (err) return console.error(err);
+
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) return console.error(err);
+
+      if (result) {
+        req.session.loggedIn = true;
+        req.session.username = username;
+        res.send('Login successful');
+      } else {
+        res.status(401).send('Incorrect password');
+      }
+    });
+  });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.send('Logged out successfully');
+});
+
+app.get('/dashboard', (req, res) => {
+  if (req.session.loggedIn) {
+    res.send(`Welcome, ${req.session.username}!`);
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'mydatabase';
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.error(err);
+
+  const db = client.db(dbName);
+  console.log('Connected to MongoDB');
+
+  db.createCollection('users', (err, res) => {
+    if (err) return console.error(err);
+    console.log('User collection created');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
